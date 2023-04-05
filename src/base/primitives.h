c37da1f66ecd518e16eab853536ac5b2ed7a8ac3
NeoCC91 <109602545+NeoCC91@users.noreply.github.com>
2023-04-05 22:33:22 +0800

    Update primitives.h

--- src/base/primitives.h
+++ src/base/primitives.h
@@ -10,22 +10,42 @@
 namespace funu
 {
 	
+	//平面直线
 	template<typename scalarType>
-	class line2{
+	class Line2{
 	public:
 		//隐式方程 ax+by+c=0
-		line2(scalarType a,scalarType b,scalarType c):dir_{-b,a}
+		Line2(scalarType a,scalarType b,scalarType c): dir_{ -b, a}
 		{
 			auto factor{static_cast<scalarType>(1)/dir_.norm()};
 			dir_=dir_*factor;
 			c_=c*factor;
+			origin_=-c_*dir_;
 		}
 		
 		//点和方向
-		line2(Vec2<scalarType> const& ori,Vec2<scalarType> const& dir):origin_{ori},dir_{dir.normalize()}{
-		
+		Line2(Vec2<scalarType> const& ori,Vec2<scalarType> const& dir): origin_{ ori}, dir_{ dir.normalize()}{
+			c_=dir_^origin_;
 		}
 		
+		//获取线上的点
+		Vec2<scalarType> const& pointOn() const{
+			return origin_;
+		}
+		//获取线方向，单位向量
+		Vec2<scalarType> const& dir() const{
+			return dir_;
+		}
+		//获取线法向(a,b)
+		Vec2<scalarType> const& normal() const{
+			return {dir_[1],-dir_[0]};
+		}
+		
+		//点到线的有向距离
+		scalarType pntDis(Vec2<scalarType> const& pnt) const
+		{
+			return pnt^dir_+c_;
+		}
 		
 	private:
 		//线上点
@@ -35,11 +55,15 @@ namespace funu
 		scalarType c_;
 	};
 	
+	using Line2f=Line2<float>;
+	using Line2d=Line2<double>;
+	
+	//3维空间直线
 	template<typename scalarType>
-	class line3
+	class Line3
 	{
 	public:
-		line3(Vec3<scalarType> const& ori,Vec3<scalarType> const& dir):origin_{ori},dir_{dir}
+		Line3(Vec3<scalarType> const& ori,Vec3<scalarType> const& dir): origin_{ ori}, dir_{ dir}
 		{
 		}
 	private:
@@ -49,8 +73,8 @@ namespace funu
 		Vec3<scalarType> dir_;
 	};
 	
-	using line3f=line3<float>;
-	using line3d=line3<double>;
+	using Line3f=Line3<float>;
+	using Line3d=Line3<double>;
 	
 	template<typename scalarType>
 	class Plane
@@ -62,6 +86,7 @@ namespace funu
 			auto factor{static_cast<scalarType>(1)/normal_.norm()};
 			normal_=normal_*factor;
 			d_=d*factor;
+			origin_=-d_*normal_;
 		}
 		
 		//点和法向
@@ -92,7 +117,8 @@ namespace funu
 			return pnt*normal_+d_;
 		}
 		
-		//线与面交点
+		//线、射线、线段与面交点
+		//面与面交线
 		
 		
 	private:
