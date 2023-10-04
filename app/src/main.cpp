0acce24f5718e21135a15c3d5a348129d3711074
NeoCC91 <109602545+NeoCC91@users.noreply.github.com>
2023-10-05 06:30:24 +0800

    Update main.cpp

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <format>
 #include <limits>
+#include <string>
+
+class TextHolder
+{
+public:
+	TextHolder(std::string text) : m_text{std::move(text)}
+	{
+	}
+
+	std::string getText() { return "guoce"; }
+
+private:
+	std::string m_text;
+};
 
 int main()
 {
-	std::cout<<std::format("{}",std::numeric_limits<float>::digits)<<std::endl;
+	std::cout << TextHolder{ "Helloxx world!" }.getText() << std::endl;
+	//std::cout << std::move(textHolder).getText() << std::endl;
 	return 0;
 }
